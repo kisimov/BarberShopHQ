847041167f622d5b38bb47b1e6985c38aaf77769
kisimov <skisimov@gmail.com>
2017-02-05 18:45:25 +0300

    display error text

--- app.rb
+++ app.rb
@@ -30,8 +30,10 @@ end
 post '/visit' do
 
 	c = Client.new params[:client]
-	c.save
-
-	erb "<h2>Спасибо, вы записались!</h2>"
-
+	if c.save
+		erb "<h2>Спасибо, вы записались!</h2>"
+	else
+		@error = c.errors.full_messages.first
+        erb :visit
+	end
 end
