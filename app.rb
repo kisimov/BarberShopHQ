f08b9d9b2514146f396bda32c1e7230dbabc4b29
kisimov <skisimov@gmail.com>
2017-02-05 16:28:56 +0300

    add visit and fix db migrate

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
@@ -9,6 +8,39 @@ set :database, "sqlite3:barbershop.db"
 class Client < ActiveRecord::Base
 end
 
+class Barber < ActiveRecord::Base
+end
+
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index			
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	# name, phone, datestamp, barber, color
+
+	c = Client.new
+	c.name = @username
+	c.phone = @phone
+	c.datestamp = @datetime
+	c.barber = @barber
+	c.color = @color
+	c.save
+
+	erb "<h2>Спасибо, вы записались!</h2>"
+
 end
